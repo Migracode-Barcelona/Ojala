7880c7fcdc786cf6dee7f22d3aa6c9ff3023d7fd
Işıl Demirtaş <isildemirtas@Isls-MacBook-Pro.local>
2023-10-19 17:40:44 +0200

    46V2

--- client/ojala-website-react/src/components/Hero/Hero.js
+++ client/ojala-website-react/src/components/Hero/Hero.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Button from 'react-bootstrap/Button';
+import headerImage from "assets/ojala-header-image.jpg";
 
 function Hero() {
 	return (
-		<Container fluid>
+		<Container fluid className="d-flex flex-column align-items-center justify-content-center">
 			<hr></hr>
-			<h1 className="display-1">Use your native language<br></br>to learn Spanish</h1>
+			<Image src={headerImage} alt="Image" className="w-25"></Image>
+			<h1 className="display-1">Use your <span className="custom-color"> native language</span><br></br>to learn <span className="custom-color">Spanish</span></h1>
+			<Button variant="success">Start Now</Button>{' '}
 		</Container>
 	);
 }
