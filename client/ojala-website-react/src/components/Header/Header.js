8c2b4b77c74c4fa02460644d511431762b4f3b6a
L0016 <l0016@L0016s-MacBook-Pro.local>
2023-10-27 19:50:31 +0200

    100V1

--- client/ojala-website-react/src/components/Header/Header.js
+++ client/ojala-website-react/src/components/Header/Header.js
@@ -4,30 +4,43 @@ import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import Dropdown from 'react-bootstrap/Dropdown';
 import ojala_logo from "assets/ojala_logo.png";
 
 function Header() {
 	return (
 		<Container fluid className="my-1">
-			<Row>
-				<Col xs={4} md={2} xl={1}>
-					<Container fluid className="text-center">
-						<Image width={"60%"} height={"60%"} src={ojala_logo} fluid />
-					</Container>
-				</Col>
+      <Row>
+        <Col xs={4} 
+		     md={2}
+			 xl={1}>
 
-				<Col
-					xs={8}
-					md={10}
-					xl={11}
-					className="d-flex flex-wrap justify-content-end align-self-center"
-				>
-					<i className="bi bi-translate icon-lg"></i>
-					<Button>عربي</Button>
-					<Button className="me-1">Castellano</Button>
-				</Col>
-			</Row>
-		</Container>
+          <Container fluid className="text-center">
+          <Image width={"60%"} height={"60%"} src={ojala_logo} fluid />
+          </Container>
+        </Col>
+
+        <Col xs={8}
+		     md={10}
+		     xl={11}
+		  className="d-flex flex-wrap justify-content-end align-self-center">
+          <Button className="me-1">Contacto</Button>
+		  <Dropdown>
+      <Dropdown.Toggle id="dropdown-basic">
+	  <i className="bi bi-translate icon-lg"></i>
+
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Castellano</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">عربي</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">English</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+		  
+        </Col>
+      </Row>
+    </Container>
 	);
 }
 
@@ -54,4 +67,36 @@ export default Header;
 			borderRadius: "5px",
 			margin: "5px",
 		}}
+
+
+		BEFORE CONTACT 
+
+		<Container fluid className="my-1">
+			<Row>
+				<Col xs={4} md={2} xl={1}>
+					<Container fluid className="text-center">
+						<Image width={"60%"} height={"60%"} src={ojala_logo} fluid />
+					</Container>
+				</Col>
+
+				<Col
+					xs={8}
+					md={10}
+					xl={11}
+					className="d-flex flex-wrap justify-content-end align-self-center"
+				>
+					<i className="bi bi-translate icon-lg"></i>
+					<Button>عربي</Button>
+					<Button className="me-1">Castellano</Button>
+				</Col>
+				<Col
+					xs={8}
+					md={10}
+					xl={11}
+					className="d-flex flex-wrap justify-content-end align-self-center"
+				>
+					<Button className="me-1">Contacto</Button>
+				</Col>
+			</Row>
+		</Container>
 	></Button>; */
