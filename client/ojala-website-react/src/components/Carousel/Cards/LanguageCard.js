cdd987698d4eda64a6e170ffdf7b84be7430a13e
OCC-0111 <occ-0111@OCC-0111s-MacBook-Pro.local>
2023-10-28 23:41:32 +0200

    101v3 cards render through an array of objects

--- client/ojala-website-react/src/components/Carousel/Cards/LanguageCard.js
+++ client/ojala-website-react/src/components/Carousel/Cards/LanguageCard.js
@@ -3,28 +3,23 @@ import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Ratio from "react-bootstrap/Ratio";
-import user4 from "assets/English-Card.png";
 
-function EnglishCard() {
+function LanguageCard(props) {
     return (
         <Col xs={12} md={10}>
             <Card className="overflow-auto">
                 <Ratio aspectRatio={150}>
-                    <Card.Img src={user4} className="object-fit-cover" />
+                    <Card.Img src={props.image} className="object-fit-cover" />
                 </Ratio>
                 <Card.ImgOverlay className="d-flex align-items-end">
                     <Container className="gray-transparent-background">
-                        <Card.Title>English</Card.Title>
-                        <Card.Text className="cards-text">
-                            This language learning platform provides an accessible and effective way
-                            for English speakers to acquire proficiency in Spanish, making the learning
-                            process engaging and enjoyable.
-                        </Card.Text>
+                        <Card.Title className="text-end">{props.title}</Card.Title>
+                        <Card.Text className="cards-text text-end">{props.text}</Card.Text>
                     </Container>
                 </Card.ImgOverlay>
             </Card>
         </Col>
-    );  
+    );
 };
 
-export default EnglishCard;
+export default LanguageCard;
